2a0da3d44187ef27838bcfb9a3bd6591589a53e8
Dmitriy <snipere@list.ru>
2023-05-02 17:50:09 +1000

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Дмитрий Лубов on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
